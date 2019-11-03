d937a19aca527cd54120942929a3b909c8ebd7c9
Naveen <kumarn.na20@gmail.com>
2019-11-03 14:53:04 +0530

    resolved error by adding abs file path

--- webcamobjdetection.py
+++ webcamobjdetection.py
@@ -3,9 +3,9 @@ import numpy as np
 import time
 
 # load yolo
-net = cv2.dnn.readNet("yolov3-tiny.weights","yolov3-tiny.cfg")
+net = cv2.dnn.readNet("D:/OpenCV Practice/yolov3-tiny.weights","D:/OpenCV Practice/yolov3-tiny.cfg")
 classes = []
-with open("coco.names", "r") as f:
+with open("D:/OpenCV Practice/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
 layer_names = net.getLayerNames()
