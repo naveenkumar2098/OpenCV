f023995bca868dfe6e0effc35005a04954d5d28d
Naveen <kumarn.na20@gmail.com>
2019-11-03 14:48:24 +0530

    added absolute file path

--- objdetection.py
+++ objdetection.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np
 
 # load yolo
-net = cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
+net = cv2.dnn.readNet("D:/OpenCV Practice/yolov3.weights","D:/OpenCV Practice/yolov3.cfg")
 classes = []
-with open("coco.names", "r") as f:
+with open("D:/OpenCV Practice/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
 layer_names = net.getLayerNames()
 outputlayers = [layer_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size = (len(classes), 3))
 #loading images
-img = cv2.imread("IMG-20181229-WA0070.jpg")
+img = cv2.imread("D:/OpenCV Practice/IMG-20181229-WA0070.jpg")
 img = cv2.resize(img, None, fx=0.7, fy=0.7)
 height, width, channels = img.shape
 
